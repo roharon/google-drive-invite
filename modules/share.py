3841f3d033ca0a6f620b61c472406e996f978938
Aaron Roh <roharon98@gmail.com>
2020-01-11 22:33:29 +0900

    modify: wrong api params

--- modules/share.py
+++ modules/share.py
@@ -6,36 +6,53 @@ import os
 """
 need to set environment variable
 
-'authorization_code'
-'client_id'
-'client_secret'
+# on get_token Funtion
+
+'client_id' : string
+'client_secret' : string
+'refresh_token' : string
+
+----
+# on share_task Function
+
+'file_id' : string
+'send_notification' : boolean
+'email_message' : string
 """
 
 def get_token():
+
     redirect_uri = request.host + "/code"
 
     headers = {
         "Content-Type": "application/x-www-form-urlencoded"
     }
 
-    REFRESH_TOKEN_URL = "www.googleapis.com/oauth2/v4/token?" \
-        + "code=" + os.environ['authorization_code'] + "&client_id=" + os.environ['client_id'] \
+    REFRESH_TOKEN_URL = "https://www.googleapis.com/oauth2/v4/token?" \
+            + "client_id=" + os.environ['client_id'] \
             + "&client_secret=" + os.environ['client_secret'] \
-            + "&redirect_uri=" + redirect_uri + "&grant_type=authorization_code"
+            + "&refresh_token=" + os.environ['refresh_token'] \
+            + "&grant_type=refresh_token"
+
 
     result = requests.post(REFRESH_TOKEN_URL, headers=headers)
 
     if(result.status_code == 200):
         result_json = result.json()
         return result_json['access_token']
+
     else:
         return "500"
 
 
-def share_task(email_address=None):
+def share_task():
 
-    file_id = os.environ('file_id')
-    PERMISSION_URL = "https://www.googleapis.com/drive/v3/files/" + file_id + "/permissions"
+    email_address = request.form['email']
+    file_id = os.environ['file_id']
+
+    PERMISSION_URL = "https://www.googleapis.com/drive/v3/files/" + file_id + "/permissions" \
+        + "&sendNotificationEmail=" + os.environ['send_notification'] \
+        + "&emailMessage=" + os.environ['email_message']
 
     token = get_token()
 

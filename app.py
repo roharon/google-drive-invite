c4b6df3e14fea0fd61fb8ddfc1bb9579492188d1
Aaron Roh <roharon98@gmail.com>
2020-01-11 23:10:47 +0900

    add: app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
-
+from modules import share
 app = Flask(__name__)
 
-@app.route("/invite")
-def invite():
-    
+
+@app.route("/invite", methods=['GET'])
+def invite_get():
     return render_template("invite.html")
+
+@app.route("/invite", methods=['POST'])
+def invite_post():
+    return share.share_task()

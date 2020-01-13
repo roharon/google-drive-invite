5462a5fd95f1e466065080f02b4a7ba18ff9ed28
Aaron Roh <roharon98@gmail.com>
2020-01-13 21:30:25 +0900

    add: error, invite page

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
-from flask import Flask, render_template, request, Response
+import os
+from flask import Flask, render_template, request, Response, redirect
 from modules.share import share_task
+
 app = Flask(__name__)
 
 
-@app.route("/invite", methods=['GET'])
 @app.route("/", methods=['GET'])
-def invite_get():
-    return render_template("invite.html")
+def home():
+    return render_template("home.html")
+
+@app.route("/invite", methods=['GET'])
+def invite_result():
+    return render_template("invite.html", file_id=os.environ['file_id'])
+
+@app.route("/error", methods=['GET'])
+def error():
+    return render_template("error.html")
 
 @app.route("/invite", methods=['POST'])
 def invite_post():
     response = share_task(email_address=request.json['email'], verify_code=request.json['verify_code'])
 
-    if response == "400":
-        return Response("not found", status=400)
-    
-    elif response == "500":
-        return Response("env error", status=500)
-
+    if (int(response)/100 == 4) or (int(response)/100==5):
+        return redirect('/error')
     else:
-        return response
+        return redirect('/invite')
 
 if __name__ == "__main__":
     app.run("0.0.0.0", "80")
